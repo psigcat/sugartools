5210dd92e1ec3e96bdbff5b60a4c092aaeb2d111
geraldo1 <geraldo@servus.at>
2025-06-11 16:13:10 +0200

    tool6: use named layer attributes instead of positions, see #6

--- tool6_extractblocks.py
+++ tool6_extractblocks.py
@@ -9,6 +9,7 @@ FIELDS_MANDATORY = ["extractblocks_folder", "extract_table", "extract_polygon_la
 RELTABLE_ID = "rel_"
 POLYGON_LAYER_ID = "_2D"
 LINE_LAYER_ID = "_lin2D"
+LINE_LAYER_ID2 = "_Lin2D"
 THREED_LAYER_ID = "_3D"
 STR_BLOQUES = "BL"
 STR_BASES_NAGATIVAS = "BN"
@@ -39,7 +40,7 @@ class ExtractblocksTool():
                     self.parent.dlg.extract_table.setLayer(layer)
                 elif POLYGON_LAYER_ID in layer_name:
                     self.parent.dlg.extract_polygon_layer.setLayer(layer)
-                elif LINE_LAYER_ID in layer_name:
+                elif LINE_LAYER_ID in layer_name or LINE_LAYER_ID2 in layer_name:
                     self.parent.dlg.extract_lines_layer.setLayer(layer)
                 elif THREED_LAYER_ID in layer_name:
                     self.parent.dlg.extract_3d_layer.setLayer(layer)
@@ -55,21 +56,24 @@ class ExtractblocksTool():
         extract_layer = QgsProject.instance().mapLayersByName(extract_table)[0]
         restricted_uas = self.parent.dlg.extract_restrict.text().split(",")
 
-        #print(restricted_uas)
-
         feature_count = len(list(extract_layer.getFeatures()))
         self.progress = self.utils.initProgressBar("Process extractioin...", feature_count)
+        #print(extract_table, feature_count)
 
         for feature in extract_layer.getFeatures():
             attributes = feature.attributes()
-            
-            id_bloque = feature[1]
-            ua = feature[2]
-            position = feature[3]
+
+            id_bloque = str(feature.attribute("id_bloque"))
+            ua = str(feature.attribute("id_UA"))
+            position = str(feature.attribute("posicion"))
+
+            #print(id_bloque, position, ua, restricted_uas, ua in restricted_uas)
 
             if len(restricted_uas) == 0 or restricted_uas[0].strip() == "" or ua in restricted_uas:
                 self.add_features(id_bloque, ua, position)
                 self.progress.setValue(self.progress.value() + 1)
+
+        #self.reload_layers()
 
         self.parent.dlg.messageBar.clearWidgets()
         self.parent.dlg.messageBar.pushMessage(f"Forms extracted and written to working directory", level=Qgis.Success)
@@ -80,14 +84,15 @@ class ExtractblocksTool():
 
         form_type = self.get_form_type()
         dir_name = f"{form_type}_{ua}"
+
         combination = f"_{ua}_{position}"
         combination_all = f"_{ua}_ALL"
-        layer_name_2d = form_type + "2D" + combination
-        layer_name_2d_all = form_type + "2D" + combination_all
-        layer_name_lines = "Lines" + combination
-        layer_name_lines_all = "Lines" + combination_all
-        layer_name_3d = form_type + "3D" + combination
-        layer_name_3d_all = form_type + "3D" + combination_all
+        layer_name_2d = form_type + "_2D" + combination
+        layer_name_2d_all = form_type + "_2D" + combination_all
+        layer_name_lines = form_type + "_lines" + combination
+        layer_name_lines_all = form_type + "_lines" + combination_all
+        layer_name_3d = form_type + "_3D" + combination
+        layer_name_3d_all = form_type + "_3D" + combination_all
 
         self.add_feature(id_bloque, layer_name_2d, "Polygon", dir_name)
         self.add_feature(id_bloque, layer_name_2d_all, "Polygon", dir_name)
@@ -103,6 +108,8 @@ class ExtractblocksTool():
         layer = self.get_layer(layer_name, geom_type, dir_name)
         feature = self.get_feature(id_bloque, geom_type)
 
+        #print("add_feature", layer, feature, id_bloque, geom_type)
+
         if layer and feature:
             self.write_feature(feature, layer, id_bloque)
 
@@ -111,6 +118,8 @@ class ExtractblocksTool():
         """ get layer from project """
 
         layer_path = os.path.join(self.parent.dlg.extractblocks_folder.filePath(), dir_name, layer_name + ".gpkg")
+
+        #print("get_layer", layer_name, layer_path, os.path.exists(layer_path))
 
         if os.path.exists(layer_path):
             layer = QgsVectorLayer(layer_path, layer_name, 'ogr')
@@ -129,7 +138,7 @@ class ExtractblocksTool():
     def create_layer(self, layer_name, geom_type, dir_name):
         """ create empty polygon 2d layer file """
 
-        layer_uri = f"{geom_type}?crs=epsg:25831&field=id_bloque:integer"
+        layer_uri = f"{geom_type}?crs=epsg:25831&field=id_bloque:string(8)"
         layer = QgsVectorLayer(layer_uri, layer_name, "memory")
 
         if self.parent.dlg.extract_check_layers.isChecked():
@@ -138,6 +147,8 @@ class ExtractblocksTool():
         path = self.parent.dlg.extractblocks_folder.filePath()
         path = os.path.join(path, dir_name)
         self.utils.make_permanent(layer, path)
+
+        #print("create_layer", layer_name, geom_type, path)
 
         return layer
 
@@ -157,8 +168,10 @@ class ExtractblocksTool():
             return None
 
         layer = QgsProject.instance().mapLayersByName(layer_name)[0]
-        request = QgsFeatureRequest(QgsExpression(f"\"id_bloque\" = {id_bloque}"))
+        request = QgsFeatureRequest(QgsExpression(f"\"id_bloque\" = '{id_bloque}'"))
         features = list(layer.getFeatures(request))
+
+        #print("get_feature", id_bloque, geom_type, layer, request, len(features))
 
         if len(features) == 0:
             return None
@@ -176,6 +189,7 @@ class ExtractblocksTool():
         layer.startEditing()
         layer.addFeature(feature)
         layer.commitChanges()
+        layer.updateExtents()
 
 
     def has_layer(self, name):
@@ -202,3 +216,14 @@ class ExtractblocksTool():
             return STR_ESTRUCTURAS
         else:
             return STR_BLOQUES
+
+
+    def reload_layers(self):
+        """ update data source to make feature count work properly """
+
+        for layer in QgsProject.instance().mapLayers().values():
+            if layer.type() == layer.VectorLayer:
+                if layer.isEditable():
+                    layer.commitChanges()
+                layer.updateExtents()
+                self.parent.iface.layerTreeView().refreshLayerSymbology(layer.id())
